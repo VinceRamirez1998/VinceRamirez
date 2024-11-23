7675e3f33485b7c0938b52f1688be833deea9b39
Vince Ramirez <it.benedictramirez247@gmail.com>
2024-11-23 21:20:56 +0800

    Fixed Changes and Allignments

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -2,23 +2,22 @@
 import { FaFacebook, FaLinkedin, FaEnvelope, FaFilePdf } from 'react-icons/fa'; // Facebook, LinkedIn, Email, Resume icons
 
 const iconLinks = [
-  { href: "/resume.pdf", icon: <FaFilePdf className="h-6 w-6" />, label: "Resume" },
-  { href: "https://www.linkedin.com/in/vince-ramirez/", icon: <FaLinkedin className="h-6 w-6" />, label: "LinkedIn" },
-  { href: "mailto:vincedev@gmail.com", icon: <FaEnvelope className="h-6 w-6" />, label: "Email" },
-  { href: "https://www.facebook.com/vince.ramirez", target: "_blank", icon: <FaFacebook className="h-6 w-6" />, label: "Facebook" }, // Facebook icon
+  { href: "/resume/Resume - Benedict Vincent Ramirez.pdf", icon: <FaFilePdf className="h-6 w-6" />, label: "Resume", download: true },
+  { href: "https://www.linkedin.com/in/lets-build-the-future/", icon: <FaLinkedin className="h-6 w-6" />, label: "LinkedIn" },
+  { href: "mailto:it.benedictramirez247@gmail.com", icon: <FaEnvelope className="h-6 w-6" />, label: "Email" },
+  { href: "https://www.facebook.com/BenedictVincentRamirez", target: "_blank", icon: <FaFacebook className="h-6 w-6" />, label: "Facebook" },
 ];
 
 const Introduction = () => {
   return (
     <section className="min-h-screen py-12 px-6 bg-white dark:bg-darkbg flex justify-center">
-      {/* Container to apply 920px width and align items at the top */}
       <div className="max-w-[var(--customwidth)] w-full flex justify-between items-start space-x-6">
         
         {/* Left Section (Name, Title, Location) */}
         <div className="flex flex-col justify-start space-y-4 text-start max-w-md">
-    <h1 className="text-5xl font-extrabold text-gray-900 dark:text-white font-calistoga tracking-tighter">Vince Ramirez</h1>
-          <p className="text-xl text-gray-700 dark:text-gray-300">Full Stack Web Developer</p>
-          <p className="text-lg text-gray-600 dark:text-gray-400">📍 Mexico, Pampanga | PH</p>
+          <h1 className="text-5xl font-extrabold text-gray-900 dark:text-white font-calistoga tracking-tighter">Vince Ramirez</h1>
+          <p className="text-md text-gray-600 dark:text-gray-400 font-sans">Full Stack Web Developer</p>
+          <p className="text-md text-gray-600 dark:text-gray-400 font-sans">Mexico, Pampanga 📍  | PH</p>
 
           {/* Social Icons with added padding-top */}
           <div className="flex space-x-6 mt-6 justify-start pt-5">
@@ -35,6 +34,7 @@ const Introduction = () => {
                     : "hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2"
                   }`}
                 aria-label={link.label}
+                download={link.download ? "Resume - Benedict Vincent Ramirez.pdf" : undefined} 
               >
                 {link.label === "Resume" ? (
                   <>
