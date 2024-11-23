1552875518084058a7a082ee2294af2b8c3dce9a
Vince Ramirez <it.benedictramirez247@gmail.com>
2024-11-23 18:42:55 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-  // pages/index.tsx or app/page.tsx
-'use client'; 
-  import Introduction from '../../components/introduction';
-  import Tablist from '../../components/tablist';
+// pages/index.tsx or app/page.tsx
 
-  export default function Home() {
-    return (
-      <div className="bg-gray-100 dark:bg-darkbg">
-        {/* Introduction Section */}
-        <Introduction />
-  
-        {/* Tablist Section */}
-        <Tablist />
-      </div>
-    );
-  }
+import Introduction from '../../components/introduction';
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Introduction/>
+      {/* Other sections of your page */}
+    </div>
+  );
+}
