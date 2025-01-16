761bdf3fce9898bc89fae973322c38b829d626a9
Vince Ramirez <it.benedictramirez247@gmail.com>
2025-01-16 17:49:12 +0800

    text white bug mobile

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -111,10 +111,8 @@ const ProjectsPage: React.FC = () => {
                   alt={item.title}
                   className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  <h3 className="text-white text-xl font-semibold mb-2">
-                    {item.title}
-                  </h3>
+                <div className="absolute hover:text-white dark:text-white inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
                   <p className="text-white text-sm text-center mb-4">
                     {/* can do item.description here */}
                   </p>
